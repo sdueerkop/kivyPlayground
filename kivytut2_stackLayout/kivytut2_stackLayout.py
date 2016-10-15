dfa1506260c4abaf9402bcbd61e1beb225867a0c
Sven Düerkop <sdueerkop@uni-bremen.de>
2016-10-15 23:17:57 +0200

    Added directory with stack layout example

---
+++ kivytut2_stackLayout/kivytut2_stackLayout.py
@@ -0,0 +1,13 @@
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.stacklayout import StackLayout
+
+class StackLayoutApp(App):
+
+    def build(self):
+        return StackLayout()
+
+slApp = StackLayoutApp()
+slApp.run()
